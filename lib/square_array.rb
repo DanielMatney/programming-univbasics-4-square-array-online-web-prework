4b03adf31f86150bcd29176322faeac296fe438a
Daniel Matney <danieljmatney@gmail.com>
2019-09-03 03:19:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
   counter = 0
-  while counter < 4
-  p array[counter]
-  counter += 1
+  while counter < array.length
+    array[counter] ** 2
+    counter += 1
+  end
 end
